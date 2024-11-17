17ef69fdd2b523fafa3f97dee62f038cfc0200c4
nazir <nazirxdfall@gmail.com>
2024-11-17 10:55:11 +0700

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,41 @@
+import { nanoid } from 'nanoid-ESM';
+import { push, filter } from './notes';
+
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    updatedAt,
+  };
+  push(newNote);
+
+  const isSuccess = filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+export default addNoteHandler;
